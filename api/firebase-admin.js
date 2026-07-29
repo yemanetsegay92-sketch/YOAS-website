c3576bae23270f6ce4686e72232a8c5dc22514d7
yemanetsegay92-sketch <yemanetsegay92@gmail.com>
2026-07-29 23:13:29 +0300

    Initialize Firebase Admin SDK if not already initialized

--- api/firebase-admin.js
+++ api/firebase-admin.js
@@ -1,1 +1,19 @@
+import admin from "firebase-admin";
 
+if (!admin.apps.length) {
+
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n");
+
+    admin.initializeApp({
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: privateKey
+        })
+    });
+
+}
+
+const db = admin.firestore();
+
+export default db;
